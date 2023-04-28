930a2233f75f655977b0b59fd6b440474471f840
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-04-28 15:00:12 +0200

    add "MILESTONE 0" in js

---
+++ assets/myscript.js
@@ -0,0 +1,54 @@
+/*
+Utilizziamo i dati forniti per creare un array di oggetti per rappresentare i membri del team.
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+*/
+
+/*
+MILESTONE 0:
+Creare l’array di oggetti con le informazioni fornite.
+*/ 
+
+const team = [
+    {
+        firstName: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        img: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        firstName: 'Angela Caroll',
+        role: 'Chief Editor',
+        img: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        firstName: 'Walter Gordon',
+        role: 'Office Manager',
+        img: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        firstName: 'Angela Lopez',
+        role: 'Social Media Manager',
+        img: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        firstName: 'Scott Estrada',
+        role: 'Developer',
+        img: 'scott-estrada-developer.jpg'
+    },
+    {
+        firstName: 'Barbara Ramos',
+        lastName: 'Graphic Designer',
+        img: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+/*
+MILESTONE 1:
+Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto.
+*/
+
+
+		
+		
+		
+		
+		
